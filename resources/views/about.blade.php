4a6731c3a7650c31458397024b729181cae4f31b
Johepdgc <johepdg07@icloud.com>
2024-04-14 21:17:42 -0600

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Us</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+    <<div class="p-3 mb-2 bg-dark-subtle text-dark-emphasis">
+    <div class ="container text-center py-3">
+        <h1>Acerca de Buddyt</h1>
+        <h2>Nuestra Historia y Misión</h2>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 py-3">
+                <p>Buddyt fue fundado en [año de fundación] con el objetivo de proporcionar a las personas las herramientas necesarias para tomar el control de sus finanzas de manera fácil y efectiva. Desde entonces, nos hemos comprometido a ofrecer una experiencia financiera personalizada y centrada en el usuario.</p>
+                <p>Nuestra misión es empoderar a las personas para que tomen decisiones financieras informadas y alcancen sus objetivos de manera inteligente. Creemos que todos merecen tener acceso a herramientas financieras sólidas que les permitan construir un futuro próspero.</p>
+            </div>
+            <div class="col-md-6">
+                <img class="rounded-4" src="{{ asset('images/accounting.png') }}" alt="team" width="500" height="300">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12 py-3">
+                <h2>Nuestro Equipo</h2>
+                <p>Detrás de Buddyt hay un equipo apasionado de profesionales dedicados a brindar la mejor experiencia posible a nuestros usuarios. Nuestro equipo está compuesto por expertos en finanzas, desarrollo de software, diseño de experiencia de usuario y más.</p>
+                <p>Estamos comprometidos a trabajar incansablemente para mejorar continuamente nuestra plataforma y proporcionar las herramientas más avanzadas para ayudar a nuestros usuarios a alcanzar sus metas financieras.</p>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+<footer>
+    <div class="container text-center py-3">
+        <p>© 2024 Buddyt. Todos los derechos reservados.</p>
+        <p>Johep Daniel Gradis Cortes</p>
+    </div>
+</footer>
+</html>
---
+++ resources/views/homePage.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+    <div class="p-3 mb-2 bg-dark-subtle text-dark-emphasis">
+    <div class ="container text-center py-3">
+        <h1>Bienvenido a Buddyt</h1>
+        <h2>Tu compañero financiero para un futuro próspero</h2>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 py-3">
+            <p>Te ayudaremos a tener control de tus finanzas con facilidad y sin problemas!</p>
+            <p>El objetivo de Buddyt es de que tomes control de tus finanzas y asi puedas ahorrar y comprender tu estado financiero de una forma sencilla y efectiva!</p>
+        </div>
+        <div class="col-md-6">
+            <img class="rounded-4" src="{{ asset('images/financialManagement.jpeg') }}" alt="management" width="500" height="300">
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-6">
+            <img class="rounded-4" src="{{ asset('images/wealth.jpeg') }}" alt="management" width="500" height="300">
+        </div>
+        <div class="col md-6 py-3">
+            <p>Con Buddyt, podrás llevar un control de tus ingresos y egresos, así como ver tus gastos por categoría y por mes. Además, nuestro sistema automatizado te ayuda a calcular tus impuestos según tu salario, para que siempre estés al día con tus obligaciones financieras.</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-6 py-3">
+        <p>Con Buddyt podras llevar un control de tus ingresos y egresos, asi como tambien podras ver tus gastos por categoria y por mes. LLevar registro de impuestos segun tu salario,
+            y ver un resumen de tus gastos e ingresos en un solo lugar.</p>
+        </div>
+        <div class="col md-6">
+            <img class="rounded-4" src="{{ asset('images/financialGrowth.jpeg') }}" alt="management" width="500" height="300">
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12 py-3">
+            <p>Queremos que nuestros usuarios puedan manejar sus finanzas de manera eficiente y tener una visión clara de su situación financiera. Regístrate ahora y comienza tu viaje hacia una mejor salud financiera con Buddyt.</p></p>
+        </div>
+    </div>
+    </div>
+</body>
+<footer>
+    <div class="container text-center py-3">
+        <p>© 2024 Buddyt. Todos los derechos reservados.</p>
+        <p>Johep Daniel Gradis Cortes</p>
+    </div>
+</footer>
+</html>
